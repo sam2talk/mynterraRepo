b3aaa850e60839f51a22df66e54c941fc80b39a2
sam2talk <samstation09@gmail.com>
2023-02-18 17:58:44 +0000

    update main

--- main.tf
+++ main.tf
@@ -63,3 +63,13 @@ tags= {
   }
 }
 
+resource "aws_s3_bucket" "my-s3-bucket" {
+  bucket_prefix = var.bucket_prefix
+  acl = var.acl
+
+   versioning {
+    enabled = var.versioning
+  }
+
+  tags = var.tags
+}
